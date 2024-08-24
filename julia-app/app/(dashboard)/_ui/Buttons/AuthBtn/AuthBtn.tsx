ae5d41fe20184e2186ecabc988c6f3d7109d904c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-08-24 22:56:56 +0300

    auth btn

--- julia-app/app/(dashboard)/_ui/Buttons/AuthBtn/AuthBtn.tsx
+++ julia-app/app/(dashboard)/_ui/Buttons/AuthBtn/AuthBtn.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React from 'react'
-import { useFormStatus, useFormState } from 'react-dom';
+import { useFormStatus } from 'react-dom';
+import "../../../_style/_ui/AuthBtn/_auth.btn.scss"
 
 const AuthBtn = ({btnText}:{btnText: string}) => {
     const { pending } = useFormStatus();
     return (
-        <button disabled={pending}>{pending ? "פרטים נשלחים..." : btnText}</button>
+        <button className="authBtn" disabled={pending}>{pending ? "פרטים נשלחים..." : btnText}</button>
     )
 }
 
