9208653f80f0f87b19dbd6fa902a75094dfb203e
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-08-26 18:33:33 +0300

    add background bottom

--- julia-app/app/(main)/components/navbar/Navbar.tsx
+++ julia-app/app/(main)/components/navbar/Navbar.tsx
@@ -2,25 +2,24 @@
 
 import Link from "next/link"
 import { navLinks } from "../../text/navLinks";
+// import { navLinks } from "../text/navLinks"
 
 
 const Navbar = () => {
   return (
-   
- 
+
+
     <nav className="
      sm:w-[100%] sm:h-[220px] sm:left-[-1px] sm:absolute sm:bg-white sm:shadow-lg sm:rounded-lg sm:p-4 sm:top-[121px] 
-
-     md:w-[720px] md:h-[50px] md:top-[122px] md:rounded-b-[30px] 
- 
+     md:w-[720px] md:h-[50px] md:top-[122px] md:rounded-b-[30px]
     ">
-           <ul className="
-           sm:flex sm:flex-col sm:space-y-4
-          
+      <ul className="
+           sm:flex sm:flex-col sm:space-y-4 
+          md:flex-row
            ">
         {navLinks.map((link, index) => (
           <li key={index}
-           className="
+            className="
             sm:text-[22px] sm:font-semibold sm:text-left
             sm:text-[#526768] sm:leading-[26.73px]
             sm:pl-4
@@ -31,9 +30,9 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-        </nav>
-     
-       
+    </nav>
+
+
   )
 }
 
--- julia-app/app/(main)/components/navbar/TopBar.tsx
+++ julia-app/app/(main)/components/navbar/TopBar.tsx
@@ -6,42 +6,42 @@ import Image from 'next/image';
 
 
 const TopBar = () => {
-    const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+  const [isNavbarVisible, setIsNavbarVisible] = useState(false);
 
- const handleToggleBar = () => {
+  const handleToggleBar = () => {
     setIsNavbarVisible((prevState) => !prevState);
-    }
+  }
 
   return (
-   <>
-   <div className="
-   sm:bg-smBackground sm:h-[121px] relative 
-   
-     lg:bg-red-500">
+    <>
+      <div className="
+       sm:bg-smBackground sm:h-[121px] relative 
+       lg:bg-red-500"
+       >
 
-     <button
-     onClick={handleToggleBar}
-     className="
+        <button
+          onClick={handleToggleBar}
+          className="
         
          sm:w-[60px] sm:h-[60px] sm:absolute sm:top-[31px] sm:right-10 
          sm:rounded-[10px] sm:bg-topBg sm:flex sm:justify-center sm:items-center
          md:hidden
          lg:hidden
         ">
-        <Image 
-        src={isNavbarVisible ? "/close.svg" : "/vector.svg"}
-        width={30}
-        height={30}
-        className="self-center"
-        alt="btn vector png"
-        />    
-      </button>
- </div>
+          <Image
+            src={isNavbarVisible ? "/close.svg" : "/vector.svg"}
+            width={30}
+            height={30}
+            className="self-center"
+            alt="btn vector png"
+          />
+        </button>
+      </div>
 
- <div className="">
-  {isNavbarVisible && <Navbar />}
- </div>
- </>
+      <div className="">
+        {isNavbarVisible && <Navbar />}
+      </div>
+    </>
   )
 }
 
