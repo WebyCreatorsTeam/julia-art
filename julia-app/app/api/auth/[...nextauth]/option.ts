6b9b9ae9edf2f546450c3f6014a390e424eedb5b
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-09-01 17:51:46 +0300

    auth?

--- julia-app/app/api/auth/[...nextauth]/option.ts
+++ julia-app/app/api/auth/[...nextauth]/option.ts
@@ -1,9 +1,41 @@
 import type { NextAuthOptions } from "next-auth"
+import credentials from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
-    providers: [],
+    session: {
+        strategy: 'jwt',
+    },
+    callbacks: {
+        async jwt({ token, account, profile }: { token: any, account: any, profile: any }) {
+            if (account && account.type === 'credentials') {
+                token.userId = account.providerAccountId; // this is Id that coming from authorize() callback
+            }
+            return token;
+        },
+        async session({ session, token, user }: any) {
+            session.user.id = token.userId;
+            return session;
+        },
+    },
     pages: {
-        signIn: "/login"
-    }
-}
+        signIn: '/login', // Set custom signin page path
+    },
+    providers: [
+        credentials({
+            name: 'Credentials',
+            credentials: {
+                userEmail: { label: "userEmail", type: "email", placeholder: "Введите E-mail" },
+                userPassword: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const { userEmail, userPassword } = credentials as {
+                    userEmail: string;
+                    userPassword: string;
+                };
 
+                return authenticate(userEmail, userPassword);
+            },
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET, // SECRET env variable
+}
--- julia-app/app/lib/dashboard/action/auth.ts
+++ julia-app/app/lib/dashboard/action/auth.ts
@@ -40,3 +40,24 @@ export const registUser = async (prevState: State, formData: FormData) => {
     revalidatePath('/dashboard/users');
     redirect('/dashboard/users');
 }
+
+import { signIn } from 'next-auth/react';
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
