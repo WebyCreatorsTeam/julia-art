0f30ca16975257db036c18a86d930c0127f70827
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-09-01 14:27:26 +0300

    login fields

--- julia-app/app/(dashboard)/_form-fields/login.ts
+++ julia-app/app/(dashboard)/_form-fields/login.ts
@@ -1,6 +1,6 @@
 import { IFormInput } from "../_ts/form-fields.intarface";
 
 export const LoginInputFileds: Array<IFormInput> = [
-    { type: "email", placeholder: "Введите E-mail", name:"userEmail" },
-    { type: "password", placeholder: "Введите Пароль", name:"userPassword" },
+    { type: "email", placeholder: "Введите E-mail", name:"userEmail", autoComp: "email" },
+    { type: "password", placeholder: "Введите Пароль", name:"userPassword" , autoComp: "current-password" },
 ]
