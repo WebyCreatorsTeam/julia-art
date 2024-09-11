20c2b1960458293cc583ecfe9749a615cfa17d6f
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2024-09-11 16:42:24 +0300

    test

---
+++ julia-app/app/(main)/components/headers/Header.tsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <div className="
+    sm:w-[375px] sm:h-[1596px]">
+   
+    </div>
+  )
+}
+
+export default Header
--- julia-app/app/(main)/layout.tsx
+++ julia-app/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TopBar from "./components/navbar/TopBar";
+import Header from "./components/headers/Header";
 
 
 
@@ -21,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
     <body className={inter.className}>
         <TopBar />
-        
+        <Header />
       
         <main>{children}</main>
       </body>
